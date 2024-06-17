0bef5189fc181753c3b39323277a2d92d50c26ad
90duse <duseibnuduse1212@gmail.com>
2024-06-17 12:51:59 +0300

    added an update

--- find_special_word.py
+++ find_special_word.py
@@ -1,9 +1,9 @@
 # this code will get you any special word that you may want 
-# to search inside a file. in the example we are using legacy.txt file which it
-# has some words.  all you need is to have the file. 
+# to search inside a file. in the example we are using legacy.txt file which
+# has some words and if you want to use it in you file you can, all you need is to have the file and upload it. 
 
 try:
-    name = input('Enter your file ')
+    name = input('Enter your file: ')
     handle  = open(name)
 
     counts = dict()
@@ -13,11 +13,7 @@ try:
             counts[word] = counts.get(word, 0) + 1
 
     handle.close() # this will close the file after reading it to free the system in some resources
-
-
     bigword = input('Enter the word you want to fine inside the file: ').strip()
-    
-
     if bigword in counts:
         total = counts[bigword]
         print('your word ', bigword , 'apears in the file times', total)
@@ -31,5 +27,9 @@ try:
         
   
 
-except :
-      print('Unkown error is found in the code ',)
+except FileNotFoundError:
+    print(f"Error: The file '{name}' was not found.")
+except PermissionError:
+    print(f"Error: Permission denied to access the file '{name}'.")
+except Exception as e:
+    print(f"An error occurred: {e}")
